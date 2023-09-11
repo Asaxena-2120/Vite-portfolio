c3c7227ab271e4c4fc6a08bdaf695954385f5e88
Aastha Saxena <saxena.aastha91@gmail.com>
2023-09-11 18:21:31 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <div className="bg-red-500">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus ex ut rerum aperiam illum ducimus molestias consectetur explicabo quibusdam? Doloribus repellendus dolor quae!
+          <div className="bg-purple-900 mx-3 text-white">this</div>
+        </div>
+      </div>
+        
+
+    </>
+  )
+}
+
+export default App
